7330161c4562e6f63528963da13642d677a8fdd4
ZhangChi <e1221818@u.nus.edu>
2025-08-07 23:10:04 +0800

    complete useraction

--- Clients/src/main/java/com/team12/clients/listing/ListingClient.java
+++ Clients/src/main/java/com/team12/clients/listing/ListingClient.java
@@ -6,8 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "listing", path = "/listing", configuration = FeignTokenRelayConfig.class)
 public interface ListingClient {
     @GetMapping("/{id}")
     PropertyDto getPropertyById(@PathVariable("id") Long id);
+
+    @GetMapping("")
+    List<PropertyDto> getAllProperties();
 }
